ece6e021fdbfcf894ec4cceaca59ac2050282a08
Tsutomu Ogasawara <tsutomu.ogasawara@mail.rakuten.com>
2014-04-22 13:24:39 +0900

    Added popup function.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,14 @@
 'use strict';
+
+(function($, moment) {
+    $(document).on('mouseup', function(e) {
+        var text = window.getSelection().toString();
+        if ( text.length > 0 ) {
+            var m = moment(text);
+            if ( m.isValid() ) {
+                var str = m.format('dddd (L)');
+                $(e.target).attr('title', str);
+            }
+        }
+    });
+})($, moment);
